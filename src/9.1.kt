712dcc1229e1d20efa2cf010a0dbef163f6a4ecc
ItachiUciha1488 <kuller_1488@mail.ru>
2021-02-11 11:22:01 +0700

    123

---
+++ src/9.1.kt
@@ -0,0 +1,39 @@
+fun main() {
+//   var x =5.0
+//   var y = 3*x.pow(6)-6*x.pow(2.0)-7
+//   println("значение функции = $x")
+
+//var x = 7.0
+//var y = 4*(x-3)*x.pow(6)-7*(x-3)*x.pow(3)+2
+//println("значение функции = $y")
+
+//   var A = readLine()!!.toDouble()
+//   var A2 = A*A
+//   var A4 = A2*A2
+//   var A8 = A4*A4
+//   print("A2 = $A2\nA4 = $A4\nA8 = $A8")
+
+//   var A = readLine()!!.toDouble()
+//   var A2 = A*A
+//   var A3 = A2*A
+//   var A5 = A3*A2
+//   var A10 = A5*A5
+//   var A15 = A10*A5
+//   print("A2 = $A2\nA3 = $A3\nA5 = $A5\nA10 = $A10\nA15 = $A15")
+//
+
+//это ламба, а это гелик, Влад это бумага, а бумага это деньги, вы не забыли как летает моя ламба, *врум,врум*
+//
+   var pi = 3.14
+   print("Введите угол в градусах: [0..360]: ")
+  var Alpha = readLine()!!.toDouble()
+   var Rad = (Alpha*pi)/180
+   print("Радианы: $Rad")//
+
+
+
+
+
+
+
+}
